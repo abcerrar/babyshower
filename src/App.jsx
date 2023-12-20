5d3439a6d2f9a5389e89f24ffe95b81caf9dd8f5
abcerrar <70103837+abcerrar@users.noreply.github.com>
2023-12-20 13:22:54 +0100

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+
+function App(){
+ 
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/">
+					<Route index element={<Home/>}/>
+					<Route path="*" element={<Home/>}/>					
+					{/* <Route path="home" element={<Home/>}/> */}				
+				</Route>
+			</Routes>
+		</BrowserRouter>
+		
+	)
+}
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import '../../style/general.scss'
+import './home.scss'
+
+export default function Home(){
+	return (
+		<div className="container">
+			<div className='home_container'>
+				<input type="text" />
+			</div>
+		</div>
+	)
+}
