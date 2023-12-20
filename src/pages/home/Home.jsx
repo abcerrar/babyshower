fa35ddfc4c2aa7d27177d8c3da07e46960e77a81
abcerrar <70103837+abcerrar@users.noreply.github.com>
2023-12-20 13:37:53 +0100

    home struct

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,11 @@ export default function Home(){
 		<div className="container">
 			<div className='home_container'>
 				<input type="text" />
+
+				<div className='button_home'><p>Unirse</p></div>
+				<div className='button_home'><p>Espectar</p></div>
+				<div className='button_home'><p>Crear</p></div>
+
 			</div>
 		</div>
 	)
